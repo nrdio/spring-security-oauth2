8a6656e7f5c40a91f8e63495c95f5fe5c3a3da98
nd <nitin.rajen.deshmukh@gmail.com>
2019-04-18 22:18:57 +0200

    Enabled global method security

--- resource-server/src/main/java/com/nrdio/resourceserver/config/ResourceServerConfig.java
+++ resource-server/src/main/java/com/nrdio/resourceserver/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.nrdio.resourceserver.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -11,6 +12,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 @EnableOAuth2Sso
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
