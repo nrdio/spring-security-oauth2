41826d17f8d1582ce99d073d833efab2eeab328c
nd <nitin.rajen.deshmukh@gmail.com>
2019-04-18 21:38:15 +0200

    Enabled resource server and added security config

---
+++ resource-server/src/main/java/com/nrdio/resourceserver/config/ResourceServerConfig.java
@@ -0,0 +1,23 @@
+package com.nrdio.resourceserver.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+@EnableOAuth2Sso
+@EnableWebSecurity
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/actuator/health").permitAll()
+                .anyRequest().authenticated();
+    }
+
+}
